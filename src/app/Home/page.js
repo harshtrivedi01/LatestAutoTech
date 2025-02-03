ac7f07dc74873f5ce04f377526616397c7aa599b
daksh-infosoft <developer15.dakashinfo@gmail.com>
2025-02-03 10:15:43 +0530

    Setting up the configuration

---
+++ src/app/Home/page.js
@@ -0,0 +1,11 @@
+
+
+export default function Homefirst () {
+
+    return  (
+        <div>
+            <h1>Home Page</h1>
+            <p> This is the home page of the site</p>
+        </div>
+    )
+}
