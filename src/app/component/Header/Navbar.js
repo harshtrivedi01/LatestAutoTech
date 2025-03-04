b12323940f4dd3a6d92216aadf2da3b1bec29d9f
developer15dakashinfo <developer15.dakashinfo@gmail.com>
2025-03-04 12:51:08 +0000

    new

--- src/app/component/Header/Navbar.js
+++ src/app/component/Header/Navbar.js
@@ -75,11 +75,13 @@ export default function Navbar() {
                   />
                   {showDropdown && (
                     <div className="absolute left-0 z-50 mt-2 w-32 bg-white border border-gray-200 rounded-lg shadow-lg">
-                      <a className="my-1 block border-b border-gray-100 px-4 py-2 font-semibold text-gray-500 hover:text-black">
-                        <Link href="/profile">  {t("Profile")}</Link>
+                      <a href="/profile"
+                       className="my-1 block border-b border-gray-100 px-4 py-2 font-semibold text-gray-500 hover:text-black">
+                        {t("Profile")}
                       </a>
-                      <a className="my-1 block border-b border-gray-100 px-4 py-2 font-semibold text-gray-500 hover:text-black">
-                        <Link href="/mybooking">  {t("booking")}</Link>
+                      <a  href="/mybooking"
+                      className="my-1 block border-b border-gray-100 px-4 py-2 font-semibold text-gray-500 hover:text-black">
+                        {t("booking")}
                       </a>
 
                       <div data-translate
